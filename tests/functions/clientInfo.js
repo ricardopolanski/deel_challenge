6b86c7477ae94e368006f71c78be42640da52d6c
ricardopolanski <rpolanski@live.com>
2022-03-16 13:45:13 -0300

    clientInfo.js created

---
+++ tests/functions/clientInfo.js
@@ -0,0 +1,11 @@
+function clientInfo() {
+    const name = "New Client Contractor"
+    const email = "newcliente@hotmail.com"
+    const password = "123456"
+    return {
+        name,
+        email,
+        password
+    };
+}
+module.exports = {clientInfo}
