10a8507da7dfaccf119630cb86f370334e50620a
ricardopolanski <rpolanski@live.com>
2022-03-16 17:56:26 -0300

    create contract refactored

--- tests/specs/createDeelContract.spec.js
+++ tests/specs/createDeelContract.spec.js
@@ -30,22 +30,66 @@ test.describe('Create Contract', () => {
       page.locator(loginElements.btnLogin()).click()
 
     ]); */
-    await page.locator(contractElements.create()).click();
-    await expect(page).toHaveURL('https://app.deel.training/create');
-    await page.locator(contractElements.fixedRate()).click();
-    await expect(page).toHaveURL('https://app.deel.training/create/fixed');
     await page.locator(contractElements.btnCookies()).click();
+    await page.locator(contractElements.btnNextPopUp()).click();
+    await page.locator(contractElements.btnNextPopUp()).click();
+    await page.locator(contractElements.btnNextPopUp()).click();
+    await page.locator(contractElements.btnDone()).click();
+
+    await expect(page.locator(contractElements.create())).toBeVisible();
+    await expect(page.locator(contractElements.create())).toBeEnabled();
+    await page.locator(contractElements.create()).click();
+
+    await expect(page).toHaveURL('https://app.deel.training/create');
+    await expect(page.locator(contractElements.fixedRate())).toBeVisible();
+    await expect(page.locator(contractElements.fixedRate())).toBeEnabled();
+    await page.locator(contractElements.fixedRate()).click();
+
+    await expect(page).toHaveURL('https://app.deel.training/create/fixed');
+    await expect(page.locator(contractElements.entityField()).nth(2)).toBeVisible();
+    await expect(page.locator(contractElements.entityField()).nth(2)).toBeEnabled();
     await page.locator(contractElements.entityField()).nth(2).click();
+
+    await expect(page.locator(contractElements.entityName())).toBeVisible();
+    await expect(page.locator(contractElements.entityName())).toBeEnabled();
     await page.locator(contractElements.entityName()).click();
+
+    await expect(page.locator(contractElements.contractName())).toBeVisible();
+    await expect(page.locator(contractElements.contractName())).toBeEnabled();
     await page.locator(contractElements.contractName()).fill(info.contracName);
+
+    await expect(page.locator(contractElements.countryField()).first()).toBeVisible();
+    await expect(page.locator(contractElements.countryField()).first()).toBeEnabled();
     await page.locator(contractElements.countryField()).first().click();
+
+    await expect(page.locator(contractElements.countryName(info.country))).toBeVisible();
+    await expect(page.locator(contractElements.countryName(info.country))).toBeEnabled();
     await page.locator(contractElements.countryName(info.country)).click();
+
+    await expect(page.locator(contractElements.provinceField())).toBeVisible();
+    await expect(page.locator(contractElements.provinceField())).toBeEnabled();
     await page.locator(contractElements.provinceField()).click();
+
+    await expect(page.locator(contractElements.provinceName(info.state))).toBeVisible();
+    await expect(page.locator(contractElements.provinceName(info.state))).toBeEnabled();
     await page.locator(contractElements.provinceName(info.state)).click();
+
+    await expect(page.locator(contractElements.jobTitle())).toBeVisible();
+    await expect(page.locator(contractElements.jobTitle())).toBeEnabled();
     await page.locator(contractElements.jobTitle()).fill(`${info.jobTitle}`);
+
+    await expect(page.locator(contractElements.seniorityField())).toBeVisible();
+    await expect(page.locator(contractElements.seniorityField())).toBeEnabled();
     await page.locator(contractElements.seniorityField()).click();
+
+    await expect(page.locator(contractElements.seniority(info.roleLevel))).toBeVisible();
+    await expect(page.locator(contractElements.seniority(info.roleLevel))).toBeEnabled();
     await page.locator(contractElements.seniority(info.roleLevel)).click();
+
+    await expect(page.locator(contractElements.scopeField())).toBeVisible();
+    await expect(page.locator(contractElements.scopeField())).toBeEnabled();
     await page.locator(contractElements.scopeField()).fill(`${info.scopeWork}`);
+
     const date = page.locator(contractElements.todayDay(getDate.month, getDate.day, getDate.year));
     await date.click();
     await page.locator(contractElements.yesterday(getDate.day)).click();
